f42e72f99c5f72b636bb6a6cbfcdc23033c9f308
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2024-08-28 22:18:32 -0600

    tile listener added

--- src/com/gitub/AmirrezaZahraei1387/GameMap/TileGB.java
+++ src/com/gitub/AmirrezaZahraei1387/GameMap/TileGB.java
@@ -9,8 +9,14 @@ a BufferedImage
  */
 public class TileGB {
     public BufferedImage img;
+    public TileListener listener;
+
+    public TileGB(BufferedImage img, TileListener listener){
+        this.img = img;
+        this.listener = listener;
+    }
 
     public TileGB(BufferedImage img){
-        this.img = img;
+        this(img, null);
     }
 }
---
+++ src/com/gitub/AmirrezaZahraei1387/GameMap/TileListener.java
@@ -0,0 +1,10 @@
+package com.gitub.AmirrezaZahraei1387.GameMap;
+
+public interface TileListener {
+
+    /*
+    called whenever this object collides with a player.
+     */
+    //TODO: add the player parameter(s) once player is implemented
+    public void onCollide();
+}
